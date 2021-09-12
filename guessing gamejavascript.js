2f88f3dbceb33776f40f54e1e6c53ab4ef9205f5
Arjan Ghosh <62652094+Arjan49@users.noreply.github.com>
2021-09-12 12:19:36 +0500

    Add files via upload

---
+++ guessing gamejavascript.js
@@ -0,0 +1,25 @@
+/* Guessing Game */
+var numberOfWins = 0
+var numberOfLost = 0
+
+// run the program for 5 times 
+for (var i = 1; i <= 5; i++) {
+
+  
+    var guessNumber = parseInt(prompt("Guess a number from 1 to 5 : "))
+
+    
+    var randomNumber = Math.floor(Math.random() * 5) + 1;
+
+
+    if (guessNumber == randomNumber) {
+        numberOfWins++;
+        console.log("You have won ")
+    }
+    else {
+        numberOfLost++;
+        console.log("You have lost. Random number was : " + randomNumber)
+    }
+}
+console.log("Total win = " + numberOfWins)
+console.log("Total lost = " + numberOfLost)
